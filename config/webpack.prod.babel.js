6c85d01a0d98ed45cd2f35052bc0d0990baa0db2
Balram Singh <singhbalram786@gmail.com>
2018-07-19 14:36:18 +0100

    Initial version of boilerplate with React 16, Redux 5.0.7 and webpack 4

---
+++ config/webpack.prod.babel.js
@@ -0,0 +1,52 @@
+// Important modules this config uses
+const path = require('path');
+// const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = require('./webpack.base.babel')({
+  mode: 'production',
+  // In production, we skip all hot-reloading stuff
+  entry: [
+    path.join(process.cwd(), 'app/app.js')
+  ],
+
+  // Utilize long-term caching by adding content hashes (not compilation hashes) to compiled assets
+  output: {
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].chunk.js'
+  },
+
+  plugins: [
+
+    // Minify and optimize the index.html
+    new HtmlWebpackPlugin({
+      filename: 'app1.html',
+      template: 'app/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
+      inject: true
+    }),
+
+    new HtmlWebpackPlugin({
+      filename: 'app2.html',
+      template: 'app/index.html',
+      inject: true,
+      chunks: ['app2', "vendor", "manifest"],
+      chunksSortMode: 'dependency'
+    }),
+  ],
+
+  performance: {
+    assetFilter: (assetFilename) => !(/(\.map$)|(^(main\.|favicon\.))/.test(assetFilename)),
+  },
+});
